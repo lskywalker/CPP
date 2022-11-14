1d83aa07b440ce15ba1ab37aaefeb59b1622bc53
Luke Smit <lukevincesmit@gmail.com>
2022-11-14 18:31:52 +0100

    Tiny Makefile change and working on ex02

---
+++ module_05/ex02/Bureaucrat.cpp
@@ -0,0 +1,102 @@
+#include "Bureaucrat.hpp"
+
+// Bureaucrat::Bureaucrat( void ): _name(""), _grade(1)
+// {
+
+// }
+
+Bureaucrat::Bureaucrat(std::string const name, int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	else
+		_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+{
+	*this = copy;
+}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &copy)
+{
+	_grade = copy._grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+
+}
+
+std::string	Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int			Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
+
+void		Bureaucrat::increment()
+{
+	if (_grade == 1)
+		throw GradeTooHighException();
+	_grade--;
+}
+
+void		Bureaucrat::decrement()
+{
+	if (_grade == 150)
+		throw GradeTooLowException();
+	_grade++;
+}
+
+void		Bureaucrat::increment(int value)
+{
+	if ((_grade - value) < 1)
+		throw GradeTooHighException();
+	else
+		_grade -= value;
+}
+
+void		Bureaucrat::decrement(int value)
+{
+	if ((_grade + value) > 150)
+		throw GradeTooLowException();
+	else
+		_grade += value;
+}
+
+void	Bureaucrat::signForm(Form &form) const
+{
+	if (form.beSigned(*this))
+		std::cout << "<" << _name << "> signed " << form.getName() << std::endl;
+	else
+		std::cout << "<" << _name << "> couldn't sign " << form.getName() << " because grade is not high enough!" << std::endl;
+}
+
+bool	Bureaucrat::executeForm(const Form &form) const
+{
+	try
+	{
+		form.checkExecuted(*this);
+		std::cout << "<" << _name << "> executes form " << form.getName() << std::endl;
+		return true;
+	}
+	catch(Form::CantExecute &e)
+	{
+		std::cout << e.what() << std::endl;
+		return false;
+	}
+	
+}
+
+std::ostream &	operator<<(std::ostream &o, Bureaucrat const &ref)
+{
+	o << ref.getName() << "[" << ref.getGrade() << "]" << std::endl;
+	return(o);
+}
---
+++ module_05/ex02/Form.cpp
@@ -0,0 +1,82 @@
+#include "Form.hpp"
+
+Form::Form(const std::string &name, int GradeToSign, int GradeToExecute) :
+	_name(name), _isSigned(false), _GradeToSign(GradeToSign), _GradeToExecute(GradeToExecute)
+{
+
+}
+
+Form::Form(const Form &copy) : _name(copy.getName()), _GradeToExecute(copy.getGradeToExecute()),
+	_GradeToSign(copy.getGradeToSign()), _isSigned(copy.isSigned())
+{
+	*this = copy;
+}
+
+Form	&Form::operator=(Form const &copy)
+{
+	_isSigned = copy._isSigned;
+	return (*this);
+}
+
+const std::string	&Form::getName( void ) const
+{
+	return (_name);
+}
+
+int		Form::getGradeToSign( void ) const
+{
+	return (_GradeToSign);
+}
+
+int		Form::getGradeToExecute( void ) const
+{
+	return (_GradeToExecute);
+}
+
+bool	Form::isSigned( void ) const
+{
+	return (_isSigned);
+}
+
+void	Form::checkExecuted(const Bureaucrat &bureaucrat) const
+{
+	if (_isSigned == false || bureaucrat.getGrade() < _GradeToExecute)
+		throw CantExecute();
+}
+
+void	Form::checkGrade( void ) const
+{
+	if (_GradeToExecute < 1 || _GradeToSign < 1)
+		throw GradeTooHighException();
+	if (_GradeToExecute > 150 || _GradeToSign > 150)
+		throw GradeTooLowException();
+}
+
+bool	Form::beSigned(const Bureaucrat &bureaucrat)
+{
+	if (bureaucrat.getGrade() <= _GradeToSign)
+	{
+		std::cout << _name << " has been signed by " << bureaucrat.getName() << std::endl;
+		_isSigned = true;
+		return true;
+	}
+	else
+	{
+		_isSigned = false;
+		std::cout << _name << " can't be signed by " << bureaucrat.getName() << " because: ";
+		throw GradeTooLowException();
+		return false;
+	}
+}
+
+Form::~Form()
+{
+
+}
+
+std::ostream &operator<<(std::ostream &o, Form const &ref)
+{
+	o << ref.getName() << " to be signed by [" <<
+			ref.getGradeToSign() << "] and executed by [" << ref.getGradeToExecute() << "]" << std::endl;
+	return (o);
+}
